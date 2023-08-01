16dba0046e1f36b1ebd65a33248703bca8d59c70
Javier CuanJuan <jcuanj@gmail.com>
2023-08-01 11:58:44 -0700

    Adding & Filling HTML Templates

--- index.js
+++ index.js
@@ -1,55 +1,112 @@
-// NODEJS
-// MANIPULATING FILES IN LOCAL COMPUTER
-
+// NODEJS SERVER API WITH ROUTES (OLD WAY)
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// ************************************************************
-// READ/WRITE FILE - SYNCHRONOUS BLOCKING WAY (Dont have Call back)
-// ************************************************************
-// READ FILE input.txt
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Fill out the cards.html template and replace the placeholders
+// /g (Global) means it will replace all occurrences of the
+// placeholders and not just the first one.
+// We created a new "output" variable because its not a good practice to
+// directly manipulate and change the value of the arguments that we
+// pass into a function
+const replaceTemplate = (template, product) => {
+  let output = template;
+  output = output.replace('{%PRODUCTNAME%}', product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-const textOut = `This is what we know about avocados: ${textIn}. \n Created on ${Date.now()}`;
-console.log(textOut);
+  // organic is missing above because organic is boolean
+  // not-organic is a class name
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-// WRITE FILE output.txt
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written');
+  return output;
+};
 
-// *********************************************************************
-// Read file data.json and execute a callback function Asyncronous
-// *********************************************************************
-fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-  // Detructuring data array
-  // const [a, b, c, d, e, f] = JSON.parse(data);
-  const dataObj = JSON.parse(data);
+// ********************************************************
+// READ HTML TEMPLATES and DATA.JSON FILE AND PARSE IT
+// ********************************************************
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
-  // console.log(`dataObj: ${data}`);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  'utf-8'
+);
 
-  // console.log(dataObj[4]);
-  console.log(dataObj);
-});
+// DATA.JSON - String ARRAY of all the products and their detail info
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
-// ************************************************************
-// READ/WRITE A FILE - ASYNCHRONOUS NON-BLOCKING WAY
-// ************************************************************
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR');
+// Converts data (type string) into an array of objects
+const dataObj = JSON.parse(data);
 
-  console.log(`data1 ${data1}`);
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(`data2 ${data2}`);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(`data3 ${data3}`);
+console.log(typeof data);
+console.log(typeof dataObj);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written ');
-      });
+// ********************************************************
+// CREATE SERVER
+// ********************************************************
+const server = http.createServer((req, res) => {
+  // pathName contains the current path in your browser
+  const pathName = req.url;
+
+  // ********************************************************
+  // ROUTES
+  // ********************************************************
+  // OVERVIEW PAGE
+  if (pathName === '/' || pathName === '/overview') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
     });
-  });
+
+    // MAP-Loop to fill out the CARDS HTML TEMPLATE
+    // We are looping over dataObj array with 5 elements
+    // For each element we will return something and it
+    // will be put into the same position in cardsHtml array
+    // .join('') - We use it because cardsHtml is an array, and
+    // we dont want an array, we want one big STRING containig
+    // all of the HTML. Wii join all elements intoa string.
+    const cardsHtml = dataObj
+      .map(element => replaceTemplate(tempCard, element))
+      .join('');
+
+    // Replace the PRODUCT_CARDS placeholder with the cardsHtml we created
+    const newOutput = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(newOutput);
+
+    // ********************************************************
+    // PRODUCT PAGE
+    // ********************************************************
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+
+    // API
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    res.end(data);
+
+    // NOT FOUND
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      // 'Content-Type': 'text/plain',
+      'my-own-header': 'Hello World',
+    });
+    res.end('<h1>Page not Found</h1>');
+  }
 });
 
-console.log('Reading data');
+// ********************************************************
+server.listen(3000, () => console.log('Listening on port 3000'));
